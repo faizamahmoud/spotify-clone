cd544c5ae3bd4142921699a639e2125d89d1165d
faizamahmoud <faiza.mahmoud@gmail.com>
2022-12-09 18:31:19 -0700

    templates directory initialized

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# Here we will be creating a class called Home and extending it from the View class
 
-# Create your views here.
+from django.shortcuts import render
+from django.views import View # <- View class to handle requests
+from django.http import HttpResponse # <- a class to handle sending a type of response
+from django.views.generic.base import TemplateView
+
+# GET request
+class Home(View):
+    def get(self, request):
+        return HttpResponse("Spotify Home") #similar to res.send()
+
+
+class About(View):
+    def get(self, request):
+        return HttpResponse("Spotify About")
