58a39f152c113078b6665f6d9b9dd2dcd75844d2
faizamahmoud <faiza.mahmoud@gmail.com>
2022-12-10 16:04:20 -0700

    add artistlist to urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ from . import views
 # this like app.use() in express
 urlpatterns = [
  path('', views.Home.as_view(), name="home"),
- path('about/', views.About.as_view(), name="about")
+ path('about/', views.About.as_view(), name="about"),
+ path('artists/', views.ArtistList.as_view(), name="artist_list")
 ]
